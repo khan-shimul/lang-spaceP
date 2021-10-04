2da22321fa2782e4b8a058a3f2d5c526e53fb1b3
shimulkhan <khanshimul196@gmail.com>
2021-10-04 13:33:26 +0600

    added hero-section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './Home.css'
+import bannerImg from '../../images/hero-img-01.png'
 
 const Home = () => {
     return (
         <div>
-            <h2>this is home</h2>
+            {/* banner section */}
+            <section className="hero-section">
+                <div className="row d-flex align-items-center mx-4">
+                    <div className="col-md-6">
+                        <h1 className="title">Learn New Languages  And Move Forward</h1>
+                        <div className="line"></div>
+                        <p className="sub-text">When it comes to learning anything, it's all about time. The US Foreign Service Institute (FSI) says that if you can study for ten hours a day, lower grouped languages take 48 days for basic fluency. Meanwhile, difficult languages should take you 72 days</p>
+                    </div>
+                    <div className="col-md-6">
+                        <img className="banner-img" src={bannerImg} alt="" />
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
